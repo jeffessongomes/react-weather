88bee5634f7e2d1606b6cee1f81aca0c89983eaa
Jeffesson Gomes <jeffessong4@gmail.com>
2020-11-07 18:44:08 -0300

    Criando componente NavBar

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import { WiCloudy } from 'react-icons/wi';
 
-import { Container } from './styles';
+import { BsSearch } from 'react-icons/bs';
+
+import { Container, Navbar } from './styles';
 
 const Home: React.FC = () => {
   return (
@@ -14,15 +16,39 @@ const Home: React.FC = () => {
 
           <div>
             <strong>Goianinha</strong>
-            <p>06:09 - Segunda, 9 Set 2020</p>
+            <p>06:09 - Monday, 9 Sep 2020</p>
           </div>
           <p>
             <WiCloudy size={74} color="#D7DBDD" />
-            Nublado
+            Cloudy
           </p>
         </div>
       </div>
-      <div />
+      <Navbar>
+        <div>
+          <input type="text" placeholder="Another location" />
+          <button type="button">
+            <BsSearch size={33} color="#FFF" />
+          </button>
+        </div>
+        <div>
+          <h2>Weather Details</h2>
+          <ul>
+            <li>
+              <span>Cloudy</span>
+              86%
+            </li>
+            <li>
+              <span>Humidity</span>
+              62%
+            </li>
+            <li>
+              <span>Wind</span>
+              8km/h
+            </li>
+          </ul>
+        </div>
+      </Navbar>
     </Container>
   );
 };
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -53,10 +53,75 @@ export const Container = styled.div`
       }
     }
   }
+`;
+
+export const Navbar = styled.div`
+  width: 650px;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start !important;
+  backdrop-filter: blur(15px);
+
+  padding: 0 70px 0 70px !important;
+
+  & > div {
+    input {
+      margin-top: 58px;
+
+      background: transparent;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #b3b6b7;
+      font-weight: normal;
+      color: #b3b6b7;
+
+      padding: 10px 0;
+      font-size: 1.4em;
+      width: 75%;
+
+      &::placeholder {
+        color: #b3b6b7;
+      }
+    }
+
+    button {
+      position: absolute;
+      right: 0;
+      top: 0;
+
+      height: 105px;
+      width: 105px;
+      border-radius: 0;
+      background: #d35400;
+      outline: none;
+      border: none;
+    }
+  }
 
   div + div {
-    background: rgba(0, 0, 0, 0.4);
-    width: 550px;
-    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    h2 {
+      padding: 50px 0;
+      font-size: 1.4em;
+    }
+
+    ul {
+      width: 100%;
+
+      li {
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+
+        & + li {
+          padding-top: 40px;
+        }
+      }
+    }
   }
 `;
